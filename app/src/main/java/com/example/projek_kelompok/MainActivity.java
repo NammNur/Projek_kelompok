5b75353ec3ca21de3e08b38908adade50e13c03f
NammNur <nammbro3@gmail.com>
2025-01-05 11:06:28 +0800

    tinggal tambahin functionnya sama beresin yang activity jadwal

--- app/src/main/java/com/example/projek_kelompok/MainActivity.java
+++ app/src/main/java/com/example/projek_kelompok/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.projek_kelompok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin ;
+    Button btnRegis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+       btnLogin = findViewById(R.id.login_button);
+       btnLogin.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, Login.class);
+               startActivity(intent);
+           }
+       });
+
+       btnRegis = findViewById(R.id.register_button);
+       btnRegis.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this, register.class);
+               startActivity(intent);
+           }
+       });
+
     }
 }
---
+++ app/src/main/java/com/example/projek_kelompok/maskapai_pesawat.java
@@ -0,0 +1,30 @@
+package com.example.projek_kelompok;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class maskapai_pesawat extends AppCompatActivity {
+
+    String[] maskapai = {
+            "Garuda Indonesia",
+            "Indonesia Air Asia",
+            "Lion Air",
+            "Nam Air"
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_maskapai_pesawat);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
